462f281fba3d6b725001ce800b0dd25e6599deaa
robert-hh <robert@hammelrath.com>
2023-02-10 08:50:06 +0100

    w60x: Use the defaults for MICROPY_PY_THREAD_GIL ...
    Derived from the setting of MICROPY_PY_THREAD.
    And allow MICROPY_PY_THREAD=x to be set at the make command.

--- ports/w60x/mpconfigport.h
+++ ports/w60x/mpconfigport.h
@@ -121,9 +121,10 @@
 #define MICROPY_PY_UERRNO                   (1)
 #define MICROPY_PY_USELECT                  (1)
 #define MICROPY_PY_UTIME_MP_HAL             (1)
+
+#ifndef MICROPY_PY_THREAD
 #define MICROPY_PY_THREAD                   (0)  // 1
-#define MICROPY_PY_THREAD_GIL               (0)  // 1
-#define MICROPY_PY_THREAD_GIL_VM_DIVISOR    (32)
+#endif
 
 // extended modules
 #ifndef MICROPY_PY_UASYNCIO
